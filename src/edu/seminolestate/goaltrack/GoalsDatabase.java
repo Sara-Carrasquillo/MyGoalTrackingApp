8af6e39e1f3d3994c5efd10fe16a30a7183c5448
Sara Carrasquillo <saracarrasquillo007@gmail.com>
2025-05-03 17:31:37 -0400

    Updated GoalsDatabase.java

--- src/edu/seminolestate/goaltrack/GoalsDatabase.java
+++ src/edu/seminolestate/goaltrack/GoalsDatabase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class GoalsDatabase {
     // Database connection details
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/goal_tracker";
+    private static final String URL = "jdbc:mysql:***********/goal_tracker";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
